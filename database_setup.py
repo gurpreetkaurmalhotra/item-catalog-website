acbab5d201cb06b7e9c99db83d96a60f640f7879
gurpreetkaurmalhotra <geetmalhotraa@gmail.com>
2017-05-11 22:39:53 +0530

    Update database_setup.py

--- database_setup.py
+++ database_setup.py
@@ -4,6 +4,7 @@ from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
+from sqlalchemy.orm import backref
  
 Base = declarative_base()
  
@@ -22,6 +23,7 @@ class Restaurant(Base):
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
     picture = Column(String(250))
+    restaurant = relationship(Restaurant, backref=backref('menu_item', cascade='all,delete'))
     user_id = Column(Integer, ForeignKey('user.id'))
     user = relationship(User)
     
